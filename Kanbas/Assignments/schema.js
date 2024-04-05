f2e6be4e26d3caa0ac0c110c7de05a993a01a706
saniya <saniyaanklesaria16@gmail.com>
2024-04-05 19:43:02 -0400

    MongoAtlas connected

--- Kanbas/Assignments/schema.js
+++ Kanbas/Assignments/schema.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const assignmentSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String},
     id: String,
     description: String,
     dueDate: Date,
